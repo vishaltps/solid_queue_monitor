c77bc71d98f0df41d98b2c4d363c00560a6070f3
Vishal Sadriya <vishalsadriya.tps@gmail.com>
2025-06-12 14:10:20 +0530

    rubocop fixes

--- app/services/solid_queue_monitor/reject_job_service.rb
+++ app/services/solid_queue_monitor/reject_job_service.rb
@@ -14,17 +14,15 @@ module SolidQueueMonitor
       failed_count = 0
 
       ids.each do |id|
-        begin
-          execution = SolidQueue::ScheduledExecution.find_by(id: id)
-          if execution
-            reject_job(execution)
-            success_count += 1
-          else
-            failed_count += 1
-          end
-        rescue StandardError
+        execution = SolidQueue::ScheduledExecution.find_by(id: id)
+        if execution
+          reject_job(execution)
+          success_count += 1
+        else
           failed_count += 1
         end
+      rescue StandardError
+        failed_count += 1
       end
 
       if success_count.positive? && failed_count.zero?
