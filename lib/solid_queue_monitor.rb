987c6075b0a5b2d28cd78eac01ef806cadd361cc
Vishal Sadriya <vishalsadriya.tps@gmail.com>
2026-03-17 10:29:12 +0530

    feat: support callable values for credentials config (#32)
    * feat: support callable values for username and password config
    
    Allows Proc/Lambda for deferred evaluation, enabling use with
    Rails.application.credentials which isn't available at initializer
    load time. Fully backward compatible with static string values.
    
    Closes #31
    
    * chore: update Gemfile.lock for v1.2.1

--- lib/solid_queue_monitor.rb
+++ lib/solid_queue_monitor.rb
@@ -6,8 +6,23 @@ require_relative 'solid_queue_monitor/engine'
 module SolidQueueMonitor
   class Error < StandardError; end
   class << self
-    attr_accessor :username, :password, :jobs_per_page, :authentication_enabled,
+    attr_writer :username, :password
+    attr_accessor :jobs_per_page, :authentication_enabled,
                   :auto_refresh_enabled, :auto_refresh_interval, :show_chart
+
+    def username
+      resolve_value(@username)
+    end
+
+    def password
+      resolve_value(@password)
+    end
+
+    private
+
+    def resolve_value(value)
+      value.respond_to?(:call) ? value.call : value
+    end
   end
 
   @username = 'admin'
